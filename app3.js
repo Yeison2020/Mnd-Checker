e51142252cffd1c85867dbcf759d24cec5a0103a
Yeison <enmanuelquiterios@gmail.com>
2022-03-03 13:13:22 -0500

    Adding node content

---
+++ app3.js
@@ -0,0 +1,13 @@
+console.log("Inicio del Programa");
+
+setTimeout(() => {
+  console.log("First time out");
+}, 3000);
+
+setTimeout(() => {
+  console.log("Second time out");
+}, 0);
+
+setTimeout(() => {
+  console.log("Third time out");
+}, 0);
